f3aa2877c96bf88b95d79acc7553b876e3ebd7e5
HomnayNghiabuon <136568601+HomnayNghiabuon@users.noreply.github.com>
2023-09-18 12:21:47 +0700

    Add files via upload

--- a.java
+++ a.java
@@ -1,15 +1,29 @@
 // Java code to illustrate print()
-import java.io.*;
+
+import java.util.Arrays;
 
 class Demo_print {
+	public static int threeSum(int[] A, int target){
+		Arrays.sort(A);
+		int n = A.length;
+		int ans = Integer.MAX_VALUE;
+		int temp;
+		for(int i = 0; i < n-2; i++){
+			int j = i + 1, k = n - 1;
+			while(j<k){
+				temp = A[i] + A[j] + A[k];
+				if(Math.abs(ans - target) > Math.abs(temp - target)) ans = temp;
+				if(ans == target) return ans;
+				if(ans < target) j++;
+				else k--;
+			}
+		}
+		return ans;
+	}
 	public static void main(String[] args)
 	{
-
-		// using print()
-		// all are printed in the
-		// same line
-		System.out.print("GfG! ");
-		System.out.print("GfG! ");
-		System.out.print("GfG! ");
+		int[] A = new int[] {44,41,50,55,22,35};
+		int target = 108;
+		System.out.println(threeSum(A, target));
 	}
 }
