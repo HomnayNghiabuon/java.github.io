5cc74f4b80ff9ba8dec0cd9cadd3c4cc2d8cbe31
HomnayNghiabuon <2251052072nghia@ou.edu.vn>
2023-09-18 12:18:36 +0700

    first commit

---
+++ a.java
@@ -0,0 +1,15 @@
+// Java code to illustrate print()
+import java.io.*;
+
+class Demo_print {
+	public static void main(String[] args)
+	{
+
+		// using print()
+		// all are printed in the
+		// same line
+		System.out.print("GfG! ");
+		System.out.print("GfG! ");
+		System.out.print("GfG! ");
+	}
+}
